7986281c9303d80d032c398e12a140d51fd1855a
uselessgoddess <dodickgod@gmail.com>
2021-04-03 15:32:54 +0300

    Added innovations from C++20

--- cpp/Platform.Hashing/Platform.Hashing.Hash.h
+++ cpp/Platform.Hashing/Platform.Hashing.Hash.h
@@ -5,15 +5,26 @@
 
 namespace Platform::Hashing
 {
+
+#ifndef __cpp_lib_concepts
     template <typename T, typename = std::void_t<>>
-    struct is_std_hashable : std::false_type { };
+    struct is_std_hashable_struct : std::false_type { };
 
     template <typename T>
-    struct is_std_hashable<T, std::void_t<decltype(std::declval<std::hash<T>>()(std::declval<T>()))>> : std::true_type { };
+    struct is_std_hashable_struct<T, std::void_t<decltype(std::declval<std::hash<T>>()(std::declval<T>()))>> : std::true_type { };
+
+    template <typename T>
+    inline constexpr bool is_std_hashable = is_std_hashable_struct<T>::value;
+#else
+    template <typename T>
+    concept is_std_hashable = requires(T object) {std::hash<T>{}(object);};
+#endif
+
+
 
     template<typename T> std::size_t Hash(T &&value)
     {
-        if constexpr (is_std_hashable<typename std::decay<T>::type>::value)
+        if constexpr (is_std_hashable<typename std::decay<T>::type>)
         {
             std::hash<typename std::decay<T>::type> hasher;
             return hasher(std::forward<T>(value));
