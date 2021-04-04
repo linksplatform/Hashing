967bc0574f9c92e75c7e1e27636b821f4b677587
uselessgoddess <dodickgod@gmail.com>
2021-04-04 18:51:42 +0300

    Small fixes

--- cpp/Platform.Hashing/Platform.Hashing.Hash.h
+++ cpp/Platform.Hashing/Platform.Hashing.Hash.h
@@ -10,26 +10,27 @@
 namespace Platform::Hashing
 {
 
-#ifndef __cpp_lib_concepts
+
     template <typename T, typename = std::void_t<>>
     struct is_std_hashable_t : std::false_type { };
 
     template <typename T>
     struct is_std_hashable_t<T, std::void_t<decltype(std::declval<std::hash<T>>()(std::declval<T>()))>> : std::true_type { };
 
+#ifndef __cpp_lib_concepts
     template <typename T>
     inline constexpr bool is_std_hashable = is_std_hashable_t<T>::value;
 
     template <typename T>
     inline constexpr bool not_std_hashable = !is_std_hashable_t<T>::value;
 #else
+
     template <typename T>
     concept is_std_hashable = requires(T object) {std::hash<T>{}(object);};
 
     template <typename T>
     concept not_std_hashable = !is_std_hashable<T>;
 #endif
-
 
     template<typename T> std::size_t RawHash(const T &value)
     {
@@ -40,7 +41,7 @@ namespace Platform::Hashing
 
     template<typename T> std::size_t Hash(const T &value)
     {
-        if constexpr (is_std_hashable<typename std::decay<T>::type>)
+        if constexpr (is_std_hashable<std::decay_t<T>>)
         {
             std::hash<typename std::decay<T>::type> hasher;
             return hasher(value);
