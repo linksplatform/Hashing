bdbae4ebcbc660ca94542561aab6be603cae96ad
Konstantin Dyachenko <konard@me.com>
2022-10-26 19:36:21 +0300

    Update cpp/Platform.Hashing.Tests/AllTests.cpp

--- cpp/Platform.Hashing.Tests/AllTests.cpp
+++ cpp/Platform.Hashing.Tests/AllTests.cpp
@@ -12,7 +12,7 @@ namespace Platform::Hashing::Tests
         std::size_t hash1 = 0;
         Combine(hash1, 42);
         ASSERT_NE(0U, hash1);
-        std::uint32_t hash2 = 0;
+        std::size_t hash2 = 0;
         Combine(hash2, 42);
         ASSERT_EQ(hash2, hash1);
     }
